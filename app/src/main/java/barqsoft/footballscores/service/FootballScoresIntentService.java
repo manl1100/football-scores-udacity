c69b8d32d7d7b6adf507ab911120e790a2c8956c
Manuel Sanchez <manuelsanchez1100@gmail.com>
2015-08-17 21:05:50 -0500

    Renamed intent service class

--- app/src/main/java/barqsoft/footballscores/service/FootballScoresIntentService.java
+++ app/src/main/java/barqsoft/footballscores/service/FootballScoresIntentService.java
@@ -28,12 +28,12 @@ import barqsoft.footballscores.R;
 /**
  * Created by yehya khaled on 3/2/2015.
  */
-public class myFetchService extends IntentService
+public class FootballScoresIntentService extends IntentService
 {
-    public static final String LOG_TAG = "myFetchService";
-    public myFetchService()
+    public static final String LOG_TAG = "ScoresIntentService";
+    public FootballScoresIntentService()
     {
-        super("myFetchService");
+        super("FootballScoresIntentService");
     }
 
     @Override
