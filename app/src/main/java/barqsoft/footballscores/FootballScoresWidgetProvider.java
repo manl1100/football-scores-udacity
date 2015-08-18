de2481f3c7e308f51394066ae4e85dab99b3e820
Manuel Sanchez <manuelsanchez1100@gmail.com>
2015-08-17 19:26:40 -0500

    Initial Football score widget provider

---
+++ app/src/main/java/barqsoft/footballscores/FootballScoresWidgetProvider.java
@@ -0,0 +1,16 @@
+package barqsoft.footballscores;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by Manuel Sanchez on 8/17/15
+ */
+public class FootballScoresWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
