e2e3d672432b534dba3b8f6f917652b673b2c9fe
Manuel Sanchez <manuelsanchez1100@gmail.com>
2015-08-17 20:49:26 -0500

    Static widget,  initial football scores IntentService

---
+++ app/src/main/java/barqsoft/footballscores/FootballScoresWidgetInfo.java
@@ -0,0 +1,10 @@
+package barqsoft.footballscores;
+
+import android.appwidget.AppWidgetProviderInfo;
+
+/**
+ * Created by Manuel Sanchez on 8/17/15
+ */
+public class FootballScoresWidgetInfo extends AppWidgetProviderInfo {
+
+}
--- app/src/main/java/barqsoft/footballscores/FootballScoresWidgetProvider.java
+++ app/src/main/java/barqsoft/footballscores/FootballScoresWidgetProvider.java
@@ -1,16 +1,42 @@
 package barqsoft.footballscores;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
 
 /**
  * Created by Manuel Sanchez on 8/17/15
  */
 public class FootballScoresWidgetProvider extends AppWidgetProvider {
 
+    public static final String ACTION_DATA_UPDATED = "action_data_updated";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            context.startService(new Intent(context, FootballScoresIntentService.class));
+        }
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        // update widgets
+        for (int id : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_football_score);
+
+            // set pending intent
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            appWidgetManager.updateAppWidget(id, views);
+        }
     }
 }
