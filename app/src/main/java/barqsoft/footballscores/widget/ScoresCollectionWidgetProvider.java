4f4c3d441f4043d46fcbb82f471883c6148eddcd
Manuel Sanchez <manuelsanchez1100@gmail.com>
2015-08-30 22:37:22 -0500

    Collection widget for football scores

---
+++ app/src/main/java/barqsoft/footballscores/widget/ScoresCollectionWidgetProvider.java
@@ -0,0 +1,64 @@
+package barqsoft.footballscores.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.MainActivity;
+import barqsoft.footballscores.R;
+
+/**
+ * Created by Manuel Sanchez on 8/30/15
+ */
+public class ScoresCollectionWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(context, getClass()));
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_scores_list_view);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        // update widgets
+        for (int id : appWidgetIds) {
+            // Instantiate the RemoteViews object for the app widget layout.
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_football_scores_collection);
+
+            // set pending intent
+            Intent launchIntent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+
+            Intent intent = new Intent(context, ScoresWidgetRemoteViewsService.class);
+            // Add the app widget ID to the intent extras.
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, id);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            // Set up the RemoteViews object to use a RemoteViews adapter.
+            // This adapter connects
+            // to a RemoteViewsService  through the specified intent.
+            // This is how you populate the data.
+            views.setRemoteAdapter(R.id.widget_scores_list_view, intent);
+
+            // The empty view is displayed when the collection has no items.
+            // It should be in the same layout used to instantiate the RemoteViews
+            // object above.
+            views.setEmptyView(R.id.widget_scores_list_view, R.id.empty_view);
+
+            appWidgetManager.updateAppWidget(id, views);
+        }
+    }
+
+}
