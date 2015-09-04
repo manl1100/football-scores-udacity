5aa4a24022dcb7b47512ff0c06d7f34a718e1527
Manuel Sanchez <manuelsanchez1100@gmail.com>
2015-09-04 18:25:18 -0500

    Created separate remoteviewsfactory implementation

---
+++ app/src/main/java/barqsoft/footballscores/widget/ScoresListRemoteViewsFactory.java
@@ -0,0 +1,54 @@
+package barqsoft.footballscores.widget;
+
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+/**
+ * Created by Manuel Sanchez on 9/4/15
+ */
+public class ScoresListRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        return null;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+}
